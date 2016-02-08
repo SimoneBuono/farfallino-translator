1e2781ad7d16a3e37b19097292c43488638ff5f6
zerooverflow <simone.buono@gmail.com>
2016-02-08 16:58:17 +0100

    Update farfallino.js

--- farfallino.js
+++ farfallino.js
@@ -2,6 +2,6 @@ var symbols = require('symbols');
 
 module.exports = {
 	transform: function(str) {
-		return str.replace(/[aieouAEIOU]/g,function(a,b){return a + 'f'});
+		return str.replace(/[aieouAEIOU]/g,function(a,b){return a + 'f' + a});
 	}
 }
